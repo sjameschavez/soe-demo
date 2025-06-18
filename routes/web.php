d7e2eee6345095150bb303d74623fbf2defc0cb0
sjameschavez <sjameschavez.2020@gmail.com>
2025-06-18 09:51:01 +0800

    routes demo

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+// GET request
+Route::get('/post', function () {
+    return "<h1>This is a post</h1>";
+});
+
+Route::get('/post/create', function () {
+    return "<h1>This is a post create page</h1>";
+});
+
+// POST request
+Route::post('/post', function () {
+    //TODO
+    return view('welcome');
+});
+
+// PATCH request — Note the {id} instead of :id
+Route::patch('/post/{id}', function ($id) {
+    //TODO
+});
+
+// DELETE request — Also use {id}
+Route::delete('/post/{id}', function ($id) {
+    //TODO
+});
